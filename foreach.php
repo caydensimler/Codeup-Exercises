ba8b6c61d0580c63a8ff9ba21e3dcd327c4d7e4a
Cayden <cayden.simler@hotmail.com>
2017-03-14 12:23:05 -0500

    Added the second part of the exercise for using is_scalar

--- foreach.php
+++ foreach.php
@@ -2,19 +2,24 @@
 
 $things = ['Sgt. Pepper', "11", null, [1,2,3], 3.14, "12 + 7", false, (string) 11];
 
-foreach ($things as $datum) {
-    if (is_int($datum)) {
-        echo "{$datum} is an integer\n";
-    } else if (is_float($datum)) {
-        echo "{$datum} is a float\n";
-    } else if (is_string($datum)) {
-    	echo "{$datum} is a string\n";
-    } else if (is_bool($datum)) {
-    	echo "{$datum} is a boolean\n";
-    } else if (is_array($datum)) {
-    	echo "{$datum} is an array\n";
-    } else if (is_null($datum)) {
-    	echo "{$datum} is null\n";
-    }
-}
+// foreach ($things as $datum) {
+//     if (is_int($datum)) {
+//         echo "{$datum} is an integer\n";
+//     } else if (is_float($datum)) {
+//         echo "{$datum} is a float\n";
+//     } else if (is_string($datum)) {
+//     	echo "{$datum} is a string\n";
+//     } else if (is_bool($datum)) {
+//     	echo "{$datum} is a boolean\n";
+//     } else if (is_array($datum)) {
+//     	echo "{$datum} is an array\n";
+//     } else if (is_null($datum)) {
+//     	echo "{$datum} is null\n";
+//     }
+// }
 
+foreach ($things as $datum) {
+	if(is_scalar($datum)) {
+		echo $datum . PHP_EOL;
+	}
+}
