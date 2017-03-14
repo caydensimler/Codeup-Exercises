771e823cfe2cd42779d3b2c494232766c8b29efa
Cayden <cayden.simler@hotmail.com>
2017-03-14 11:16:30 -0500

    Changed the wording of the values set.

--- for.php
+++ for.php
@@ -7,21 +7,16 @@ fwrite(STDOUT, "Give me a number range!" . PHP_EOL);
 
 fwrite(STDOUT, "Minimum? ");
 $min = trim(fgets(STDIN));
-fwrite(STDOUT, "Minimum number set to $min" . PHP_EOL);
-
-	fwrite(STDOUT, "" . PHP_EOL);
 
 fwrite(STDOUT, "Maximum? ");
 $max = trim(fgets(STDIN));
-fwrite(STDOUT, "Maximum number set to $max" . PHP_EOL);
-
-	fwrite(STDOUT, "" . PHP_EOL);
 
 fwrite(STDOUT, "What do you want to count by? ");
 $countBy = trim(fgets(STDIN));
-fwrite(STDOUT, "Counting by $countBy" . PHP_EOL);
 
 	fwrite(STDOUT, "" . PHP_EOL);
+
+fwrite(STDOUT, "Counting from $min to $max by increments of $countBy" . PHP_EOL);
 
 if (is_numeric($min) == 1 && is_numeric($max) == 1 && is_numeric($countBy) == 1) {
 	if (isset($countBy) == true) {
