690b36aa03b78ff5060f22f3f6f65becc3f65837
Cayden <cayden.simler@hotmail.com>
2017-03-15 12:28:52 -0500

    Added the first part of the exercise to establish functions for add, subtract, multiply, and divide, as well as an echo to test each function individually.

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,28 @@
 <?php
 
-function add($a, $b)
-{
+function add($a, $b) {
     return $a + $b;
 }
 
-function subtract($a, $b)
-{
-    // Add code here
+function subtract($a, $b) {
+    return $a - $b;
 }
 
-function multiply($a, $b)
-{
-    // Add code here
+function multiply($a, $b) {
+    return $a * $b;
 }
 
-function divide($a, $b)
-{
-    // Add code here
+function divide($a, $b) {
+    return $a / $b;
 }
 
 // Add code to test your functions here
+
+
+echo add(2, 2) . PHP_EOL;
+
+echo subtract(10, 5) . PHP_EOL;
+
+echo multiply(4, 4) . PHP_EOL;
+
+echo divide(6, 3) . PHP_EOL;
