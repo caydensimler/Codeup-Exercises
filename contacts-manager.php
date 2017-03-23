ad6613ad470075423fb7d72637262e4a6b1e7465
Cayden <cayden.simler@hotmail.com>
2017-03-23 11:30:05 -0500

    Finished the contacts manager application for command line.

--- contacts-manager.php
+++ contacts-manager.php
@@ -11,16 +11,30 @@ function retrieveContacts($filename) {
 	fclose($handle);
 
 	$listOfContacts = explode("\n", $contents);
+
+	sort($listOfContacts);
 	
 	foreach ($listOfContacts as $key => $contact) {
 		$tempArray = explode("|", $contact);
 		$contacts[$key]["name"] = $tempArray[0];
-		$phone = substr($tempArray[1], 0, 3) . "-" . substr($tempArray[1], 3, 3) . "-" . substr($tempArray[1], 6);
+		if (strlen($tempArray[1]) === 10) {
+			$phone = substr($tempArray[1], 0, 3) . "-" . substr($tempArray[1], 3, 3) . "-" . substr($tempArray[1], 6);
+		} else if (strlen($tempArray[1]) === 7) {
+			$phone = substr($tempArray[1], 0, 3) . "-" . substr($tempArray[1], 3, 4);
+		}
 		$contacts[$key]["number"] = $phone;
 	}
 
+	echo "/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\" . PHP_EOL;
+	echo "\033[0;35m\tNAME\t |\t NUMBER\033[0m" . PHP_EOL;
+	echo "\\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/" . PHP_EOL;
 	foreach ($contacts as $key => $contact) {
-		echo $contact['name'] . " | " . $contact['number'] . PHP_EOL;
+
+		$contactName = "\033[0;35m" . $contact['name'] . "\033[0m";
+		$contactNumber = "\033[0;35m" . $contact['number'] . "\033[0m";
+		// echo "\033[0;35m{str_pad($contact['name'], 15)} | {$contact['number']}\033[0m" . PHP_EOL;
+		echo str_pad($contactName, 28) . "|" . "    " . $contactNumber . PHP_EOL;
+		// var_dump($contactName) . PHP_EOL;
 	}
 
 	echo "" . PHP_EOL;
@@ -45,55 +59,89 @@ function createContact($filename){
 		$phoneNumber = trim(fgets(STDIN));
 
 	// Assigns the contact information to a new contact each time the function runs
-	$newContact = PHP_EOL . $firstName . " " . $lastName . "|" . $phoneNumber;
-	fwrite(STDOUT, "Is the information correct (yes/no)?  " . $newContact . PHP_EOL);
+	$newContact = $firstName . " " . $lastName . "|" . $phoneNumber;
+	return $newContact;
+
+
+}
+
+function addContactVerification($newContact, $filename) {
+	fwrite(STDOUT, "Is the information correct (yes/no)?\033[01;33m $newContact\033[0m" . PHP_EOL);
 	$informationCheck = trim(fgets(STDIN));
 
 	// Displays the new contact information and prompts the user for a yes/no answer. If yes, then create the contact and add it to the contact file. If no, then have the user type in the information again.
 	if ($informationCheck == "yes") {
-		fwrite(STDOUT, "Contact created!" . PHP_EOL);
+		fwrite(STDOUT, "Contact created!\033[0;32m $newContact \033[0m");
+		// fwrite(STDOUT,  . PHP_EOL);
 		$handle = fopen($filename, 'a');
-		fwrite($handle, $newContact);
+		fwrite($handle, PHP_EOL . $newContact);
 		fclose($handle);
-	} else {
+	} else if ($informationCheck == "no") {
 		fwrite(STDOUT, "Re-write information." . PHP_EOL);
-		createContact($filename);
+		addContactVerification(createContact($filename), $filename);
+	} else {
+		echo " " . PHP_EOL;
+		echo "Cancelled." . PHP_EOL;
+		userOptions($filename);
 	}
 }
 
 
 function searchContacts($contacts){
-	fwrite(STDOUT, "Search the contact by first name:" . PHP_EOL);
+	fwrite(STDOUT, "Search the contact by name: ");
 	$nameSearch = trim(fgets(STDIN));
 
-	
-		// var_dump($nameSearch);
-	// var_dump($nameSearch);
-	foreach ($contacts as $key => $contact) {
-		// var_dump($contact);
-		// foreach ($contact as $array => $information) {
-			if ((in_array($nameSearch, $contact)) !== false) {
-				echo "SUCCESS";
-			// } else if ($contact['name'] !== $nameSearch) {
-			} else if ((in_array($nameSearch, $contact)) === false) {
-				echo "FAILURE";
-			}
-		// }
-	}
+    foreach ($contacts as $key => $contact) {
+        if (preg_match('('.$nameSearch.')', $contact['name'])) {
+            echo "\033[0;34m" . $contact['name'] . " | " . $contact['number'] . "\033[0m" . PHP_EOL;
+        }
+    }
 }
 
+// echo "\033[01;34m" . $contact['name'] . " | " . $contact['number'] . "\033[0m" . PHP_EOL;
 
-function deleteContact(){
+function deleteContact($filename){
 
+    $handle = fopen($filename, 'r');
+    $contacts = fread($handle, filesize($filename));
+    $contacts = trim($contacts);
+    fclose($handle);
+    $tempArray = explode("\n", $contacts);
+
+    fwrite(STDOUT, "Please enter contact name to delete:" . PHP_EOL);
+    $nameDelete = trim(fgets(STDIN));
+
+    foreach ($tempArray as $key => $value) {
+        if (preg_match('('.$nameDelete.')', $value)) {
+
+        	fwrite(STDOUT, "Are you sure you want to delete \033[0;31m $value \033[0m? (yes/no)" . PHP_EOL);
+        	$confirmDeletion = trim(fgets(STDIN));
+        	if ($confirmDeletion == "yes") {
+	            array_splice($tempArray, $key, 1);
+	            echo "Contact Deleted! \033[0;31m $value \033[0m" . PHP_EOL;
+	        } else if ($confirmDeletion == "no") {
+	        	userOptions($filename);
+	        }
+        }
+    }
+
+
+    $string = implode("\n", $tempArray);
+    $handle = fopen($filename, 'w');
+    fwrite($handle, "$string");
+    fclose($handle);
+    clearstatcache();
 }
 
 
 function userOptions($filename){
-	$optionsArray = ["PLEASE ENTER AN OPTION", " ", "1: VIEW ALL CONTACTS", "2: ADD A NEW CONTACT", "3: SEARCH FOR A CONTACT BY NAME", "4: DELETE AN EXISTING CONTACT", "5: EXIT"];
+	$optionsArray = ["PLEASE ENTER AN OPTION", " ", "\033[0;35m1: VIEW ALL CONTACTS\033[0m", "\033[0;32m2: ADD A NEW CONTACT\033[0m", "\033[0;34m3: SEARCH FOR A CONTACT BY NAME\033[0m", "\033[0;31m4: DELETE AN EXISTING CONTACT\033[0m", "5: EXIT"];
 
 	foreach ($optionsArray as $option){
 		fwrite(STDOUT, $option . PHP_EOL);
 	}
+
+	$optionSelection = "";
 
 	$optionSelection = trim(fgets(STDIN));
 
@@ -108,7 +156,7 @@ function userOptions($filename){
 			break;
 		// Adds a new contact
 		case 2: 
-			createContact($filename);
+			addContactVerification(createContact($filename), $filename);
 			fwrite(STDOUT, " " . PHP_EOL);
 			fwrite(STDOUT, "____________________" . PHP_EOL);
 			userOptions($filename);
@@ -122,7 +170,7 @@ function userOptions($filename){
 			break;
 		// Deletes an existing contact
 		case 4: 
-			echo "Test 4" . PHP_EOL;
+			deleteContact($filename);
 			fwrite(STDOUT, " " . PHP_EOL);
 			fwrite(STDOUT, "____________________" . PHP_EOL);
 			userOptions($filename);
@@ -135,9 +183,5 @@ function userOptions($filename){
 	}
 }
 
-
-
 userOptions('contacts.txt');
-
-
 
